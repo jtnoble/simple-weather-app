04818256785893802e7ffc5a3b841e1f71d10163
niclane6 <niclane6@hotmail.com>
2025-11-17 00:35:35 -0600

    Added Card class and temps to all cards

--- js/main.js
+++ js/main.js
@@ -8,12 +8,25 @@ submitButtonMainPage.addEventListener("click", () => {
     console.log(`Clicked Submit with State: ${state.value} and City: ${city.value}.`);
 }); 
 
+// Formats city names seperated by whitespace into a format that can be used with geo API
+async function formatCity(city) {
+    const cityArray = city.split(' ');
+    let citySplit = "";
+    for (word of cityArray) {
+        word === cityArray.at(-1) ?
+        citySplit += `${word}` :
+        citySplit += `${word}+`;
+    }
 
-// Cards
-async function fetchPoint() {
+    return citySplit;
+}
+
+// API's
+async function fetchPoint(city) {
+    const formatedCity = await formatCity(city);
 
     try {
-        const response = await fetch(`https://nominatim.openstreetmap.org/search?q=Los+Angeles&format=jsonv2`)
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${formatedCity}&format=jsonv2`);
 
         
         if (!response.ok) {
@@ -67,23 +80,66 @@ async function fetchForcastHourly(stationAPI) {
     }
 }
 
+// Getters
 function getCurrentTemp(data) {
     return data.properties.periods[0].temperature;
 }
 
-// Testing
-async function test() {
-    const point = await fetchPoint();
-
-    const station = await fetchWeather(point.lat, point.lon);
-
-    const current = await fetchForcastHourly(station);
-
-    const temp = getCurrentTemp(current);
-
-    const card1 = document.getElementById("popular-places-card-1");
-
-    card1.innerHTML = `${temp}° F`;
+// Formatters
+function formatTemp(temp) {
+    return `${temp}° F`;
 }
 
-test();
+class Card {
+
+    #point;
+    #lat;
+    #lon;
+    #forcastHourly;
+    #temp;
+
+    constructor(city) {
+        this.city = city;
+    }
+
+    async init() {
+        this.#point = await fetchPoint(this.city);
+        this.#lat = this.#point.lat;
+        this.#lon = this.#point.lon;
+
+        this.#forcastHourly = await fetchForcastHourly(await fetchWeather(this.#lat, this.#lon));
+        this.#temp = await getCurrentTemp(this.#forcastHourly);
+
+    }
+
+    getTemp() {
+        return this.#temp;
+    }
+}
+
+async function _init() {
+
+    // Iniialize front page cards 
+    const card1 = document.getElementById("popular-places-card-1");
+    const card1Obj = new Card("Los Angeles");
+    await card1Obj.init();
+
+    const card2 = document.getElementById("popular-places-card-2");
+    const card2Obj = new Card("Denver");
+    await card2Obj.init();
+
+    const card3 = document.getElementById("popular-places-card-3");
+    const card3Obj = new Card("Chicago");
+    await card3Obj.init();
+
+    const card4 = document.getElementById("popular-places-card-4");
+    const card4Obj = new Card("New York City");
+    await card4Obj.init();
+
+    card1.innerHTML = formatTemp(card1Obj.getTemp());
+    card2.innerHTML = formatTemp(card2Obj.getTemp());
+    card3.innerHTML = formatTemp(card3Obj.getTemp());
+    card4.innerHTML = formatTemp(card4Obj.getTemp());
+}
+
+_init();
