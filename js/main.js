44109d5eb94556b8b85cb3b9cdca122c5dba1634
niclane6 <niclane6@hotmail.com>
2025-11-19 17:27:42 -0600

    Updated location API calls to take states preventing ambigious calls

--- js/main.js
+++ js/main.js
@@ -22,11 +22,11 @@ async function formatCity(city) {
 }
 
 // API's
-async function fetchPoint(city) {
+async function fetchPoint(city, state) {
     const formatedCity = await formatCity(city);
 
     try {
-        const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${formatedCity}&format=jsonv2`);
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?city=${formatedCity}&state=${state}&format=jsonv2`);
 
         
         if (!response.ok) {
@@ -98,12 +98,13 @@ class Card {
     #forcastHourly;
     #temp;
 
-    constructor(city) {
+    constructor(city, state) {
         this.city = city;
+        this.state = state;
     }
 
     async init() {
-        this.#point = await fetchPoint(this.city);
+        this.#point = await fetchPoint(this.city, this.state);
         this.#lat = this.#point.lat;
         this.#lon = this.#point.lon;
 
@@ -121,22 +122,22 @@ async function _init() {
 
     // Iniialize front page cards 
     const card1 = document.getElementById("popular-places-card-1");
-    const card1Obj = new Card("Los Angeles");
+    const card1Obj = new Card("Los Angeles", "CA");
     await card1Obj.init();
     card1.innerHTML = formatTemp(card1Obj.getTemp());
 
     const card2 = document.getElementById("popular-places-card-2");
-    const card2Obj = new Card("Denver");
+    const card2Obj = new Card("Denver", "CO");
     await card2Obj.init();
     card2.innerHTML = formatTemp(card2Obj.getTemp());
 
     const card3 = document.getElementById("popular-places-card-3");
-    const card3Obj = new Card("Chicago");
+    const card3Obj = new Card("Chicago", "IL");
     await card3Obj.init();
     card3.innerHTML = formatTemp(card3Obj.getTemp());
 
     const card4 = document.getElementById("popular-places-card-4");
-    const card4Obj = new Card("New York City");
+    const card4Obj = new Card("New York City", "NY");
     await card4Obj.init();
     card4.innerHTML = formatTemp(card4Obj.getTemp());
 }
